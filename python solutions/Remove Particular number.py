b952a58d0eb7a3266ab8c6595cf33c03d23ac958
Veteran0411 <somlinghajji@gmail.com>
2024-04-05 18:39:51 +0530

    updated  removenumber.py

--- python solutions/Remove Particular number.py
+++ python solutions/Remove Particular number.py
@@ -3,11 +3,18 @@
 # (Visteon, Persistent)
 a=[1,2,2,3,4,6,6,7]
 j=0
-for i in a:
-    if i==6:
-        pass
-    else:
-        a[j]=i
-        j+=1
-for i in range(j):
-    print(a[i],end=" ")
+
+# 1 method
+# for i in a:
+#     if i==6:
+#         pass
+#     else:
+#         a[j]=i
+#         j+=1
+# for i in range(j):
+#     print(a[i],end=" ")
+
+# 2 method
+while 6 in a:
+    a.remove(6)
+print(a)

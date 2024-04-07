3b0a73785cfb206d53f6b140e34e4fb252c9744b
Veteran0411 <somlinghajji@gmail.com>
2024-04-07 12:17:23 +0530

    modified firstUniquecharacter.js

--- javaScript solutions/firstUniqueCharacter.js
+++ javaScript solutions/firstUniqueCharacter.js
@@ -7,10 +7,20 @@ var firstUniqChar = function(s) {
         dic[i] = (dic[i] || 0) + 1;
     }
 
-    // Find the first unique character
-    const uniqueChar = Object.keys(dic).find(key => dic[key] === 1); // check the use of find method
+    // console.log(120||0,"adhahd") // answer is 120
 
-    return uniqueChar !== undefined ? s.indexOf(uniqueChar) : -1;
+    // Find the first unique character
+    // const uniqueChar = Object.keys(dic).find(key => dic[key] === 1); // check the use of find method
+
+    // using for loop
+    for(let [i,j] of Object.entries(dic)){
+        if(j===1){
+            return s.indexOf(i);
+        }
+    }
+    console.log(dic)
+    return -1;
+    // return uniqueChar !== undefined ? s.indexOf(uniqueChar) : -1;
 };
 
 console.log(firstUniqChar("ssomling"));

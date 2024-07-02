b6653a5af29602536e3893f026905efa4f43502b
Veteran0411 <somlinghajji@gmail.com>
2024-07-02 11:37:23 +0530

    modified secondmaximum.py

--- python solutions/SecondMaximum.py
+++ python solutions/SecondMaximum.py
@@ -1,23 +1,25 @@
-a=[19,19,93,92,92,3,12,45,23]
-maxi=a[0]
+import copy
+a=[19,19,93,92,92,3,12,45,23,99]
+copyA=list(set(a))
+maxi=copyA[0]
 secondMaxi=0
-
 trav=1
 
 #bubble sort loop
-for i in range(len(a)):
-    for j in range(len(a)-1):
-        if a[j]>a[j+1]:
-            a[j],a[j+1]=a[j+1],a[j]
+for i in range(len(copyA)):
+    for j in range(len(copyA)-1):
+        if copyA[j]>copyA[j+1]:
+            copyA[j],copyA[j+1]=copyA[j+1],copyA[j]
 
 
-for i in range(1,len(a)):
-    if a[i]!=a[i-1]:
-        a[trav]=a[i]
+for i in range(1,len(copyA)):
+    if copyA[i]!=copyA[i-1]:
+        copyA[trav]=copyA[i]
         trav+=1
-print(a)
+print(copyA)
 
-print("second maximum is: ",a[trav-2],trav)
+print("second maximum is: ",copyA[trav-2])
+
 # loop to find second largest number
 # for i in a:
 #     if i>maxi:
